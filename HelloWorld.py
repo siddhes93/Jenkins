429acc84ee24ad946ef36363669a6cd856baf453
siddhes93 <103021940+siddhes93@users.noreply.github.com>
2022-06-14 18:40:15 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 # This Phython program will print Hellow World...
 print("Hello World Demo...08 Feb 2022\n")
-print("Hello World GitTigger POll SCM Demo \n")
+print("Hello World GitTigger POll SCM Demo\n")
 print("Hello World Github webhook Trigger POlling SCM Demo\n")
 
 
